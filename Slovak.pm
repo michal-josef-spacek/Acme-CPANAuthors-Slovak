c19911635e349e3c0519cb8ac734a1a3e4fb0bc8
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-06-24 15:21:37 +0200

    Prepare for 0.05 version

--- Slovak.pm
+++ Slovak.pm
@@ -4,7 +4,7 @@ use strict;
 use utf8;
 use warnings;
 
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Modules.
 use Acme::CPANAuthors::Register(
@@ -116,6 +116,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
